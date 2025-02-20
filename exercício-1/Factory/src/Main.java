2944411abe231d3a254ec54f016324226829bc41
[200~git config --global user.email seuemail@exemplo.com <kroosbr278@gmail.com>
2025-02-20 17:31:43 -0300

    add fabrica de veiculos a combustao

--- exercício-1/Factory/src/Main.java
+++ exercício-1/Factory/src/Main.java
@@ -1,26 +1,43 @@
-import Fabrica.FabricaEletricos;
 import Fabrica.FabricaVeiculo;
+import Fabrica.FabricaEletricos;
+import Fabrica.FabricaCombustao;
 import Models.Veiculo;
 
 public class Main {
     public static void main(String[] args) {
-        FabricaVeiculo fabricaEletricos = new FabricaEletricos();
-        Veiculo veiculo = null;
+        String tipoFabrica = "Combustao";
         String tipoVeiculo = "Carro";
-        String marca  = "BMW";
+        String marca = "BMW";
 
-        if (tipoVeiculo.equals("Carro")) {
-            veiculo = fabricaEletricos.criarCarro(marca);
+        FabricaVeiculo fabricaVeiculos = null;
+
+        if ("Eletricos".equalsIgnoreCase(tipoFabrica)) {
+            fabricaVeiculos = new FabricaEletricos();
+        } else if ("Combustao".equalsIgnoreCase(tipoFabrica)) {
+            fabricaVeiculos = new FabricaCombustao();
+        } else {
+            System.out.println("Tipo de fábrica não suportada: " + tipoFabrica);
+            return;
         }
 
-        if (tipoVeiculo.equals("Moto")) {
-            veiculo = fabricaEletricos.criarMoto(marca);
+        Veiculo veiculo = null;
+
+        switch (tipoVeiculo) {
+            case "Carro":
+                veiculo = fabricaVeiculos.criarCarro(marca);
+                break;
+            case "Moto":
+                veiculo = fabricaVeiculos.criarMoto(marca);
+                break;
+            case "Navio":
+                veiculo = fabricaVeiculos.criarNavio(marca);
+                break;
+            default:
+                System.out.println("Tipo de veículo não suportado: " + tipoVeiculo);
         }
 
-        if (tipoVeiculo.equals("Navio")) {
-            veiculo = fabricaEletricos.criarNavio(marca);
+        if (veiculo != null) {
+            System.out.println("Veículo criado: " + veiculo);
         }
-        
-        System.out.println(veiculo);
     }
 }
